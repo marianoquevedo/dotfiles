05e9d35070d7329417a1fea21c54757f04c80be1
Mariano Quevedo <12384762+marianoquevedo@users.noreply.github.com>
2023-04-20 01:07:25 +0200

    add common mac settings install script

---
+++ install.sh
@@ -0,0 +1,342 @@
+#!/usr/bin/env bash
+
+###########################
+# This script installs the dotfiles and runs all other system configuration scripts
+# @author Adam Eivy
+###########################
+
+# include my library helpers for colorized echo and require_brew, etc
+source ./lib_sh/echos.sh
+source ./lib_sh/requirers.sh
+
+bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
+
+#####
+# install homebrew (CLI Packages)
+#####
+
+running "checking homebrew install"
+brew_bin=$(which brew) 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
+  action "installing homebrew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    if [[ $? != 0 ]]; then
+      error "unable to install homebrew, script $0 abort!"
+      exit 2
+  fi
+else
+  ok
+  # Make sure we’re using the latest Homebrew
+  running "updating homebrew"
+  brew update
+  ok
+  bot "before installing brew packages, we can upgrade any outdated packages."
+  read -r -p "run brew upgrade? [y|N] " response
+  if [[ $response =~ ^(y|yes|Y) ]];then
+      # Upgrade any already-installed formulae
+      action "upgrade brew packages..."
+      brew upgrade
+      ok "brews updated..."
+  else
+      ok "skipped brew package upgrades.";
+  fi
+fi
+
+#####
+# install brew cask (UI Packages)
+#####
+running "checking brew-cask install"
+output=$(brew tap | grep cask)
+if [[ $? != 0 ]]; then
+  action "installing brew-cask"
+  require_brew caskroom/cask/brew-cask
+fi
+brew tap caskroom/versions > /dev/null 2>&1
+ok
+
+###############################################################################
+bot "Configuring General System UI/UX..."
+###############################################################################
+# Close any open System Preferences panes, to prevent them from overriding
+# settings we’re about to change
+running "closing any system preferences to prevent issues with automated changes"
+osascript -e 'tell application "System Preferences" to quit'
+ok
+
+##############################################################################
+# Security                                                                   #
+##############################################################################
+
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+################################################
+bot "Standard System Changes"
+################################################
+running "Disable the sound effects on boot"
+sudo nvram SystemAudioVolume=" ";ok
+
+running "Expand save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true;ok
+
+running "Expand print panel by default"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true;ok
+
+running "Save to disk (not to iCloud) by default"
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false;ok
+
+running "Disable the “Are you sure you want to open this application?” dialog"
+defaults write com.apple.LaunchServices LSQuarantine -bool false;ok
+
+running "Remove duplicates in the “Open With” menu (also see 'lscleanup' alias)"
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user;ok
+
+running "Display ASCII control characters using caret notation in standard text views"
+# Try e.g. `cd /tmp; unidecode "\x{0000}" > cc.txt; open -e cc.txt`
+defaults write NSGlobalDomain NSTextShowsControlCharacters -bool true;ok
+
+running "Disable automatic termination of inactive apps"
+defaults write NSGlobalDomain NSDisableAutomaticTermination -bool true;ok
+
+running "Disable the crash reporter"
+defaults write com.apple.CrashReporter DialogType -string "none";ok
+
+running "Set Help Viewer windows to non-floating mode"
+defaults write com.apple.helpviewer DevMode -bool true;ok
+
+running "Reveal IP, hostname, OS, etc. when clicking clock in login window"
+sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName;ok
+
+running "Restart automatically if the computer freezes"
+sudo systemsetup -setrestartfreeze on;ok
+
+running "Disable smart quotes as they’re annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false;ok
+
+running "Disable smart dashes as they’re annoying when typing code"
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false;ok
+
+###############################################################################
+bot "Trackpad, mouse, keyboard, Bluetooth accessories, and input"
+###############################################################################
+
+running "Trackpad: enable tap to click for this user and for the login screen"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1;ok
+
+running "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3;ok
+
+running "Use scroll gesture with the Ctrl (^) modifier key to zoom"
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144;ok
+running "Follow the keyboard focus while zoomed in"
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true;ok
+
+running "Disable press-and-hold for keys in favor of key repeat"
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false;ok
+
+running "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 10;ok
+
+# running "Disable auto-correct"
+# defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false;ok
+
+###############################################################################
+bot "Configuring the Screen"
+###############################################################################
+
+running "Require password immediately after sleep or screen saver begins"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0;ok
+
+running "Save screenshots to the desktop"
+defaults write com.apple.screencapture location -string "${HOME}/Documents/screenshots";ok
+
+running "Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)"
+defaults write com.apple.screencapture type -string "png";ok
+
+###############################################################################
+bot "Finder Configs"
+###############################################################################
+running "Disable window animations and Get Info animations"
+defaults write com.apple.finder DisableAllAnimations -bool true;ok
+
+# running "Show hidden files by default"
+# defaults write com.apple.finder AppleShowAllFiles -bool true;ok
+
+# running "Show all filename extensions"
+# defaults write NSGlobalDomain AppleShowAllExtensions -bool true;ok
+
+running "Show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true;ok
+
+running "Show path bar"
+defaults write com.apple.finder ShowPathbar -bool true;ok
+
+running "Allow text selection in Quick Look"
+defaults write com.apple.finder QLEnableTextSelection -bool true;ok
+
+running "Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true;ok
+
+running "When performing a search, search the current folder by default"
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf";ok
+
+running "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false;ok
+
+running "Enable spring loading for directories"
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true;ok
+
+running "Remove the spring loading delay for directories"
+defaults write NSGlobalDomain com.apple.springing.delay -float 0;ok
+
+running "Avoid creating .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true;ok
+
+running "Use list view in all Finder windows by default"
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv";ok
+
+running "Disable the warning before emptying the Trash"
+defaults write com.apple.finder WarnOnEmptyTrash -bool false;ok
+
+running "Expand the following File Info panes: “General”, “Open with”, and “Sharing & Permissions”"
+defaults write com.apple.finder FXInfoPanesExpanded -dict \
+  General -bool true \
+  OpenWith -bool true \
+  Privileges -bool true;ok
+
+###############################################################################
+bot "Dock & Dashboard"
+###############################################################################
+
+running "Set the icon size of Dock items to 36 pixels"
+defaults write com.apple.dock tilesize -int 36;ok
+
+running "Change minimize/maximize window effect to scale"
+defaults write com.apple.dock mineffect -string "scale";ok
+
+running "Minimize windows into their application’s icon"
+defaults write com.apple.dock minimize-to-application -bool true;ok
+
+running "Enable spring loading for all Dock items"
+defaults write com.apple.dock enable-spring-load-actions-on-all-items -bool true;ok
+
+running "Show indicator lights for open applications in the Dock"
+defaults write com.apple.dock show-process-indicators -bool true;ok
+
+running "Don’t animate opening applications from the Dock"
+defaults write com.apple.dock launchanim -bool false;ok
+
+running "Speed up Mission Control animations"
+defaults write com.apple.dock expose-animation-duration -float 0.1;ok
+
+running "Remove the auto-hiding Dock delay"
+defaults write com.apple.dock autohide-delay -float 0;ok
+running "Remove the animation when hiding/showing the Dock"
+defaults write com.apple.dock autohide-time-modifier -float 0;ok
+
+running "Automatically hide and show the Dock"
+defaults write com.apple.dock autohide -bool true;ok
+
+running "Make Dock icons of hidden applications translucent"
+defaults write com.apple.dock showhidden -bool true;ok
+
+###############################################################################
+bot "Configuring Safari & WebKit"
+###############################################################################
+
+running "Set Safari’s home page to ‘about:blank’ for faster loading"
+defaults write com.apple.Safari HomePage -string "about:blank";ok
+
+running "Enable Safari’s debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true;ok
+
+running "Make Safari’s search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false;ok
+
+running "Remove useless icons from Safari’s bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()";ok
+
+running "Enable the Develop menu and the Web Inspector in Safari"
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true;ok
+
+running "Add a context menu item for showing the Web Inspector in web views"
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true;ok
+
+###############################################################################
+bot "Terminal & iTerm2"
+###############################################################################
+
+# running "Only use UTF-8 in Terminal.app"
+# defaults write com.apple.terminal StringEncodings -array 4;ok
+#
+# running "Use a modified version of the Solarized Dark theme by default in Terminal.app"
+# TERM_PROFILE='Solarized Dark xterm-256color';
+# CURRENT_PROFILE="$(defaults read com.apple.terminal 'Default Window Settings')";
+# if [ "${CURRENT_PROFILE}" != "${TERM_PROFILE}" ]; then
+# 	open "./configs/${TERM_PROFILE}.terminal";
+# 	sleep 1; # Wait a bit to make sure the theme is loaded
+# 	defaults write com.apple.terminal 'Default Window Settings' -string "${TERM_PROFILE}";
+# 	defaults write com.apple.terminal 'Startup Window Settings' -string "${TERM_PROFILE}";
+# fi;
+
+#running "Enable “focus follows mouse” for Terminal.app and all X11 apps"
+# i.e. hover over a window and start `typing in it without clicking first
+defaults write com.apple.terminal FocusFollowsMouse -bool true
+#defaults write org.x.X11 wm_ffm -bool true;ok
+running "Installing the Solarized Light theme for iTerm (opening file)"
+open "./configs/Solarized Light.itermcolors";ok
+running "Installing the Patched Solarized Dark theme for iTerm (opening file)"
+open "./configs/Solarized Dark Patch.itermcolors";ok
+
+running "Don’t display the annoying prompt when quitting iTerm"
+defaults write com.googlecode.iterm2 PromptOnQuit -bool false;ok
+running "hide tab title bars"
+running "animate split-terminal dimming"
+defaults write com.googlecode.iterm2 AnimateDimming -bool true;ok
+defaults write com.googlecode.iterm2 HotkeyChar -int 96;
+defaults write com.googlecode.iterm2 HotkeyCode -int 50;
+defaults write com.googlecode.iterm2 FocusFollowsMouse -int 1;
+defaults write com.googlecode.iterm2 HotkeyModifiers -int 262401;
+running "Make iTerm2 load new tabs in the same directory"
+/usr/libexec/PlistBuddy -c "set \"New Bookmarks\":0:\"Custom Directory\" Recycle" ~/Library/Preferences/com.googlecode.iterm2.plist
+ok
+
+###############################################################################
+bot "Activity Monitor"
+###############################################################################
+
+running "Show the main window when launching Activity Monitor"
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true;ok
+
+running "Visualize CPU usage in the Activity Monitor Dock icon"
+defaults write com.apple.ActivityMonitor IconType -int 5;ok
+
+running "Show all processes in Activity Monitor"
+defaults write com.apple.ActivityMonitor ShowCategory -int 0;ok
+
+running "Sort Activity Monitor results by CPU usage"
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0;ok
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+bot "OK. Note that some of these changes require a logout/restart to take effect. Killing affected applications (so they can reboot)...."
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+  "Dock" "Finder" "Mail" "Messages" "Safari" "SizeUp" "SystemUIServer" \
+  "iCal" "Terminal"; do
+  killall "${app}" > /dev/null 2>&1
+done
+
+
+bot "Woot! All done. Kill this terminal and launch iTerm"
